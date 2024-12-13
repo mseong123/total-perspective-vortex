be8a2fba316760b3ece390382a062ab2083ed79a
mseong <mseong123@hotmail.com>
2024-12-13 14:42:22 +0800

    pretty sure logistic regression doesn't work

--- test.py
+++ test.py
@@ -26,35 +26,67 @@ def main() -> None:
         os.mkdir("plot/")
     except FileExistsError:
         pass
-    y = np.array([])
-    X = np.array([[]])
-    for i in range(3):
-        experiment_no:int = 3 + i + (4 * i)
-        raw = mne.io.read_raw_edf(f"./data/files/S001/S001R{'0' if experiment_no < 10 else ''}{experiment_no}.edf",preload=True)
-        configure_channel_location(raw)
-        raw_filtered:mne.io.Raw = raw.copy().filter(8, 40)
-        epochs:mne.Epochs = mne.Epochs(raw_filtered, tmin=0, tmax=raw_filtered.annotations.duration.mean(),baseline=None,preload=True)
 
-        mask:list[bool]=[True if id == 'T0' else False for id in raw_filtered.annotations.description]
-        epochs.drop(mask)
-        y = np.hstack((y, [str(id) for id in raw_filtered.annotations.description if id == 'T1' or id == 'T2']))
-        if i == 0:
-            X = epochs.get_data().reshape(epochs.get_data().shape[0],-1)
+    result = []
+    for i in range(1,110):
+        y = np.array([])
+        X = np.array([[]])
+        prefix=""
+        if i < 10:
+            prefix = "00"
+        elif i < 100:
+            prefix = "0"
         else:
-            X = np.vstack((X,epochs.get_data().reshape(epochs.get_data().shape[0],-1)))
+            prefix = ""
+        for j in range(3):
+            experiment_no:int = 3 + j + (3 * j)
+            raw = mne.io.read_raw_edf(f"./data/files/S{prefix}{str(i)}/S{prefix}{str(i)}R{'0' if experiment_no < 10 else ''}{experiment_no}.edf",preload=True)
+            configure_channel_location(raw)
+            raw_filtered:mne.io.Raw = raw.copy().filter(8, 40)
+            # baseline=None means using entire epoch as baseline for correction, if use baseline=(0,0) no baseline correction
+            epochs:mne.Epochs = mne.Epochs(raw_filtered, tmin=0, tmax=raw_filtered.annotations.duration.mean(),baseline=(0,0),preload=True)
+            y = np.hstack((y, [str(id) for id in raw_filtered.annotations.description if id == 'T1' or id == 'T2']))
+            if (len(epochs.get_data())==29):
+                y=y[:-1]
 
+            mask:list[bool]=[True if id == 'T0' else False for id in raw_filtered.annotations.description]
+            epochs.drop(mask)
+           
+            if j == 0:
+                X = epochs.get_data().reshape(epochs.get_data().shape[0],-1)
+            else:
+                X = np.vstack((X,epochs.get_data().reshape(epochs.get_data().shape[0],-1)))
+
+        pca = PCA(random_state=42)
+        X=pca.fit_transform(X)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
+        # Don't need put random_state for StratifiedKFold because no shuffling takes place before splitting, samples split as it is.
+        CV = StratifiedKFold(n_splits=10)
+        LR = LogisticRegressionCV(random_state=42, cv=CV, refit=False)
+        LR.fit(X_train,y_train)
+        # print(LR.scores_)
+        print(LR.score(X_test, y_test)) 
+        result.append(LR.score(X_test, y_test))
+        # result.append(LR.predict(X_test))
+        # print("iter",LR.n_iter_)
+
+    print(result)
+    print(np.array(result).mean())
+
+
+
+    # raw = mne.io.read_raw_edf(f"./data/files/S004/S004R04.edf",preload=True)
+    # configure_channel_location(raw)
+    # raw_filtered = raw.copy().filter(8, 40)
+    # print(raw.get_data().shape)
+    # epochs:mne.Epochs = mne.Epochs(raw_filtered, tmin=0, tmax=np.round(raw_filtered.annotations.duration.mean(),2) - 0.01,baseline=(None,None),preload=True)
+    # X = epochs.get_data().reshape(epochs.get_data().shape[0],-1)
+    # y = raw_filtered.annotations.description
     # All below random_state is for different reasons
-    pca = PCA(random_state=42)
-    X=pca.fit_transform(X)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y)
-    # Don't need put random_state for StratifiedKFold because no shuffling takes place before splitting, samples split as it is.
-    CV = StratifiedKFold(n_splits=5)
-    LR = LogisticRegressionCV(random_state=42, cv=CV)
-    LR.fit(X_train,y_train)
-    print(LR.scores_)
-    print(LR.score(X_train, y_train))
-    print(LR.predict(X_train))
-    
+    # # print("Cs",LR.Cs_)
+    # print("Coefficients",LR.coef_.shape)
+    # print("Clases",LR.classes_)
+
     # plotting
     # fig = raw.plot(block=True, clipping=None, scalings={"eeg":70e-6}, show=False)
     # fig.savefig(f"plot/raw.png", format="png")
