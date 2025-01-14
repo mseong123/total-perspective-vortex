32b32fbd46ac3ac972a8750b72c5cfc9fb10aeea
mseong <mseong123@hotmail.com>
2025-01-14 18:58:29 +0800

    everything works - min_sample_split, entropy, depth, more than 2 classes

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ import pandas as pd
 def main():
 
     X = pd.DataFrame(np.array([[8,3,-10], [1,1,-2], [2,5,-1], [4,3,-2],[1,10,-1], [0,1,-10]]), columns=["first", "second","third"])
-    y = np.array([0,1,0,1,0,1])
+    y = np.array([2,1,0,0,1,2])
     # clf = tree.DecisionTreeClassifier(criterion="gini")
     # clf.fit(X,y)
     # print(clf.tree_.threshold)
     # print(clf.tree_.impurity)
     # tree.plot_tree(clf)
     # plt.show()
-    clf = DecisionTreeClassifier(criterion='gini', max_depth=1)
+    clf = DecisionTreeClassifier(criterion='entropy',min_samples_split=5)
     clf.fit(X, y)
     
 
--- test1.py
+++ test1.py
@@ -8,8 +8,8 @@ import pandas as pd
 def main():
 
     X = np.array([[8,3,-10], [1,1,-2], [2,5,-1], [4,3,-2],[1,10,-1], [0,1,-10]])
-    y = np.array([0,1,0,1,0,1])
-    clf = tree.DecisionTreeClassifier(criterion="gini", max_depth=1)
+    y = np.array([2,1,0,0,1,2])
+    clf = tree.DecisionTreeClassifier(criterion="entropy",min_samples_split=5)
     clf.fit(X,y)
     print("n_node_samples",clf.tree_.n_node_samples)
     print("threshold",clf.tree_.threshold)
